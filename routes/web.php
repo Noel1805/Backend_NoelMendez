10b9f472851acf4b99125b3fe3c589cc5a4f0006
Noel Santiago Mendez Jaimes <nmendez643@unab.edu.co>
2026-02-10 15:01:11 -0500

    PHP Basic

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,58 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/mi.ruta.a', function () {
-    return 'Hola esta es mi ruta';
-});
-
 Route::get('/php-basico', function () {
-    return 'Estoy aprendiendo php';
+
+    $nombre = "Noel Santiago ";
+    $apellido = "Mendez Jaimes";
+    $age =rand(10,54);
+    $height = 1.77;
+    $islogin = (bool) rand (0,1);
+
+    $nombre_completo = $nombre . $apellido;
+    echo $nombre_completo;
+
+    echo "<h1>-------Estrcutura de datos-------</h1>";
+    $message = "<br> Soy $nombre_completo tengo $age años";
+    echo $message;
+
+
+    if ($age <18 ){
+        $message = $message . "Soy menor de edad";
+    } else if ($age > 50 ){
+        $message = $message . "Soy mayor de edad";
+    } else{
+                $message = "Esta viejo";
+    }
+
+    echo $message;
+    $message .= " " .($islogin ? "Estoy Loguenado" : "No estas Loguenado");
+
+    echo "<h1>-------Funciones-------</h1>";
+
+
+    echo printUser($nombre_completo, $age);
+
+    $productsName = ["Computador", "Teclado", 25, true, false];
+    $teclado = [
+        "name" => "teclado hp",
+        "marca" => "hp",
+        "precio" => 20000,
+        "distribucion" => [
+            "lat",
+            "mx" ,
+            "use"]
+    ];
+    
+    $tecaldo["marca"] = "LG";
+    echo $tecaldo["marca"];
+
+    foreach ($productsName as $ites){
+        echo $ites;
+    }
 });
+
+function printUser(string $name, int $age){
+    return "<br> $name tiene $age años";
+}
 
